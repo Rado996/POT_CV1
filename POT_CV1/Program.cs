bbd759aebbaf7d78bbbe61e815fafb97b8a09231
Radovan Filický <rado.filickyp@gmail.com>
2021-10-02 21:26:23 +0200

    Add project files.

---
+++ POT_CV1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace POT_CV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            if (args.Length == 0 )
+            {
+                Console.Write("Zadajte meno:");
+                name = Console.ReadLine();
+            }
+            else
+            {
+                name = args[0];                
+            }
+            Console.WriteLine($"{"Hello"}.{name}");
+        }
+    }
+}
---
+++ POT_DU1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace POT_DU1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //if (!System.IO.Directory.Exists(@"C:\Users\radof\Source\Repos\POT_CV1"))
+            //{
+            //    System.IO.Directory.CreateDirectory(@"C:\Users\Public\TestFolder\");
+            //}
+            
+            System.IO.Directory.SetCurrentDirectory(@"C:\Users\radof\Source\Repos\POT_CV1");
+
+            string currentDirName = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirName);
+
+            string[] files = System.IO.Directory.GetFiles(currentDirName);
+            
+
+            foreach (string s in files)
+            {
+                System.IO.FileInfo fi = null;
+                try
+                {
+                    fi = new System.IO.FileInfo(s);
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                Console.WriteLine("{0} : {1}", fi.Name, fi.Directory);
+            }
+        }
+    }
+}
